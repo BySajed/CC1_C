a944e9bf6201a7ee33eae41c0ba1ba6dd3b53eff
Sajed BEN YOUSSEF <127627616+BySajed@users.noreply.github.com>
2024-09-25 14:54:41 +0200

    add gitignore

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * CC 1 de C
+ *
+ * Objectif: Faire une base de donnée en incluant le principe d'arbre binaire
+ * Permettre à l'utilisateur de fairedes insert et des select
+ *
+ * fait par BEN YOUSSEF Sajed 3SI2
+ *
+ * Début le 25/09/2024
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
