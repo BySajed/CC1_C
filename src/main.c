a2e3514ffe674a26ae7e25b3b9f41c6bdf1b6b71
Bysajed <sajed.benyoussef@hotmail.com>
2024-10-01 19:20:52 +0200

    add exploring function of a binary tree

--- src/main.c
+++ src/main.c
@@ -45,6 +45,16 @@ void handleUserInput(char* userInput) {
     }
 }
 
+void inorderTraversal(Node* root){
+    if (root == NULL) {
+	return;
+    }
+    
+    inorderTraversal(root->left);
+    printf("%d", root->data);
+    inorderTraversal(root->right);
+}
+
 int main() {
     
     menu();
--- src/main.h
+++ src/main.h
@@ -1,10 +1,12 @@
 // main.h
 #ifndef MAIN_H
 #define MAIN_H
+#include "structure.h"
 
 void menu();
 char *getInput(char* input);
 void handleUserInput(char *userInput);
+void inorderTraversal(Node* root);
 int main();
 
 #endif
