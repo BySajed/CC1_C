820c423e11848c2022232c443ae2772099ece8e4
Sajed BEN YOUSSEF <127627616+BySajed@users.noreply.github.com>
2024-11-15 10:51:54 +0100

    fix overflow memory try1

--- src/table.c
+++ src/table.c
@@ -7,7 +7,7 @@
 Table** tables = NULL;
 int numTables = 0;
 int tableCapacity = 5;
-char tableName[50];
+char *tableName = NULL; // Utilisation de char * pour un nom de table dynamique
 
 void resizeTableArray() {
     tableCapacity = (tableCapacity == 0) ? 1 : tableCapacity * 2;
@@ -19,7 +19,11 @@ void createTable(const char *table_name) {
         resizeTableArray();
     }
     tables[numTables] = (Table *)malloc(sizeof(Table));
+
+    // Allocation dynamique pour le nom de la table
+    tables[numTables]->name = (char *)malloc(strlen(table_name) + 1);
     strcpy(tables[numTables]->name, table_name);
+
     tables[numTables]->tree = NULL;
     numTables++;
     printf("Table %s created.\n", table_name);
@@ -38,6 +42,7 @@ void deleteTable(const char* tableName) {
     for (int i = 0; i < numTables; i++) {
         if (strcmp(tables[i]->name, tableName) == 0) {
             free(tables[i]->tree); // Assuming tree is dynamically allocated
+            free(tables[i]->name); // Libération de l'allocation dynamique pour le nom
             free(tables[i]);
             for (int j = i; j < numTables - 1; j++) {
                 tables[j] = tables[j + 1];
